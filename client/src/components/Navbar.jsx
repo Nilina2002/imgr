0f4d16abf0015f887c4e74f785cf8d7490e1cba4
Nilina Nilaksha <nilina610@gmail.com>
2025-06-27 22:14:37 +0530

    credit_display error

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets' // Adjust the path as necessary
 import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
-    const { user, setShowLogin } = useContext(AppContext);
+    const { user, setShowLogin, logOut, credit } = useContext(AppContext);
     const navigate = useNavigate();
     return (
         <div className='flex items-center justify-between py-4'>
@@ -15,16 +15,16 @@ const Navbar = () => {
             <div>
                 {user ?
                     <div className='flex gap-2 sm:gap-5 items-center'>
-                        <button onClick={() => navigate('/but-credit')} className='flex items-center gap-2 bg-blue-100 px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-300'>
+                        <button onClick={() => navigate('/buy-credit')} className='flex items-center gap-2 bg-blue-100 px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-300'>
                             <img className='w-5' src={assets.credit_star} alt="" />
-                            <p className='text-sm sm:text-xm font-medium text-gray-600'>Credits Left: 50</p>
+                            <p className='text-sm sm:text-xm font-medium text-gray-600'>Credits Left: {credit}</p>
                         </button>
-                        <p className='text-gray-600 max-sm:hidden pl-4'>Hi, Nilina!</p>
+                        <p className='text-gray-600 max-sm:hidden pl-4'>Hi, {user.name}!</p>
                         <div className='relative group'>
                             <img className='w-10 drop-shadow' src={assets.profile_icon} alt="" />
                             <div className='absolute hidden group-hover:block top-0 right-0 text-black rounded pt-12'>
                                 <ul className='list-none m-0 p-2 bg-gray-50 rounded-lg text-sm'>
-                                    <li className='py-1 px-2 cursor-pointer pr-10 hover:bg-gray-200 rounded-lg'>Logout</li>
+                                    <li onClick={logOut} className='py-1 px-2 cursor-pointer pr-10 hover:bg-gray-200 rounded-lg'>Logout</li>
                                 </ul>
                             </div>
                         </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,20 +1,59 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 export const AppContext = createContext();
 
-const AppContextProvider = (props) => {
+export const AppContextProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [showLogin, setShowLogin] = useState(false);
+    const [token, setToken] = useState(localStorage.getItem('token'));
+    const [credit, setCredit] = useState(0);
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
+
+    const loadCreditsData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/user/credits', {
+                headers: { token }
+            });
+
+            if (data.success) {
+                setCredit(data.credits);
+                setUser(data.user);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response?.data?.message || 'Error loading credits data');
+
+        }
+    };
+
+    const logOut = () => {
+        localStorage.removeItem('token');
+        setToken(null);
+        setUser(null);
+    };
+
+    useEffect(() => {
+        if (token) {
+            loadCreditsData();
+        }
+    }, [token]);
 
     const value = {
-        user, setUser, showLogin, setShowLogin
-    }
+        user,
+        setUser,
+        showLogin,
+        setShowLogin,
+        backendUrl,
+        token,
+        setToken,
+        credit,
+        setCredit,
+        loadCreditsData,
+        logOut,
+    };
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
-
-export default AppContextProvider;
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
